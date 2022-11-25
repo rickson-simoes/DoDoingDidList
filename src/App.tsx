9ae676898afd31979850ce8e1a3e53d91c742935
Rickson Simoes <rickson.simoes@hotmail.com>
2022-11-25 18:34:12 -0300

    :art: Global: font color/ App.tsx: :art: Task List design/ FormModule: :fire: :art: Css enhancement

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { Form } from './components/Form'
 import { Header } from './components/Header'
 
+import { ClipboardText } from 'phosphor-react';
+import Clipboard from './assets/Clipboard.svg';
+
 import styles from './App.module.css';
 
 export function App() {
@@ -8,10 +11,33 @@ export function App() {
     <>
       <Header />
 
-      <main className={styles.main}>
-        <Form />
+      <div className={styles.wrap}>
+        <main className={styles.main}>
+          <Form />
 
-      </main>
+          <div className={styles.tasksContainer}>
+            <div className={styles.tasksDashboard}>
+              <div className={styles.tasksCreated}>
+                <strong>Created tasks</strong>
+                <span className={styles.tasksCounter}>0</span>
+              </div>
+
+              <div className={styles.tasksFinished}>
+                <strong>Finished</strong>
+                <span className={styles.tasksCounter}>0</span>
+              </div>
+            </div>
+
+
+            <div className={styles.noTasks}>
+              <img src={Clipboard} />
+              <strong>You still don't have any task</strong>
+              <span>Create some tasks and organize your Todo's 😎</span>
+            </div>
+
+          </div>
+        </main>
+      </div>
     </>
   )
 }
