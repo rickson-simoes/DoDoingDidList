9428644404a6ff106d12d44a1aaed1b7d633edd5
Rickson Simoes <rickson.simoes@hotmail.com>
2022-11-26 13:11:19 -0300

    :abc: Form: Criar -> Create

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -31,7 +31,7 @@ export function Form({ handleSubmitForm }: IForm) {
         />
 
         <button type="submit">
-          Criar <PlusCircle />
+          Create <PlusCircle />
         </button>
       </form>
     </>
